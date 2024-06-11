bb02f0b3b2845d58a89eb4520618af5060678760
buhleradzilani <radzilanibuhle@gmail.com>
2024-06-11 19:43:25 +0200

    Initial Commit

---
+++ ExpensesTracker/ExpensesTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpensesTrackerApp.swift
+//  ExpensesTracker
+//
+//  Created by Buhle Radzilani on 2024/06/11.
+//
+
+import SwiftUI
+
+@main
+struct ExpensesTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
